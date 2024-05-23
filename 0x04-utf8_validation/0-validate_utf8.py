56d85a2924e6484a3b8b1623c9330b50f026a626
The-alpha-male <ronnymunene@outlook.com>
2024-05-23 16:20:32 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-"""method that determines if a given
-data set represents a valid UTF-8 encoding"""
+#!/usr/bin/python3
+"""0. UTF-8 Validation
+"""
 
 
 def validUTF8(data):
