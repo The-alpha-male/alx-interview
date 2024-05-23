00b251c67124d87bc88bd6e5fc4a2fa7c0b4c24a
The-alpha-male <ronnymunene@outlook.com>
2024-05-23 16:15:20 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,41 +4,31 @@ data set represents a valid UTF-8 encoding"""
 
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding."""
-    remaining_bytes = 0
+    """Function that determines if a given data set
+    represents a valid UTF-8 encoding.
+    """
+    num_bytes = 0
 
-    mask1 = 0b10000000  # 1 << 7
-    mask2 = 0b01000000  # 1 << 6
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-    for byte in data:
-        if remaining_bytes == 0:
-            leading_ones = count_leading_ones(byte)
-            if leading_ones == 0:
+    for num in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
+
+            if num_bytes == 0:
                 continue
 
-            if leading_ones == 1 or leading_ones > 4:
+            if num_bytes == 1 or num_bytes > 4:
                 return False
 
-            remaining_bytes = leading_ones - 1
         else:
-            if not is_valid_continuation_byte(byte, mask1, mask2):
+            if not (num & mask_1 and not (num & mask_2)):
                 return False
 
-        remaining_bytes -= 1
+        num_bytes -= 1
 
-    return remaining_bytes == 0
-
-
-def count_leading_ones(byte):
-    """Counts the number of leading 1 bits in a byte."""
-    mask = 0b10000000  # 1 << 7
-    count = 0
-    while byte & mask:
-        count += 1
-        mask >>= 1
-    return count
-
-
-def is_valid_continuation_byte(byte, mask1, mask2):
-    """Checks if a byte is a valid UTF-8 continuation byte."""
-    return byte & mask1 and not (byte & mask2)
+    return num_bytes == 0
