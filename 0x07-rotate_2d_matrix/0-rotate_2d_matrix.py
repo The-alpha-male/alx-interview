09fcfebb9f894ed264d8e73f8f154753263ae530
The-alpha-male <ronnymunene@outlook.com>
2024-06-14 02:00:49 +0300

    Create a scipt that rotates a 2D matrix by 90 degrees clockwise and tested it

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Rotate 2D Matrix by 90 degrees clockwise."""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotate 2D Matrix by 90 degrees clockwise"""
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in matrix:
+        row.reverse()
+    
