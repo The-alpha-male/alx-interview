fa1f69f6bf00e1bcb2af1a7f68abb3e90e5c7ccf
The-alpha-male <ronnymunene@outlook.com>
2024-06-14 02:03:21 +0300

    Create a scipt that rotates a 2D matrix by 90 degrees clockwise and integrated tested it

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python3
-"""Rotate 2D Matrix by 90 degrees clockwise."""
+#!/usr/bin/python3
+"""0. Rotate 2D Matrix
+"""
 
 
 def rotate_2d_matrix(matrix):
-    """Rotate 2D Matrix by 90 degrees clockwise"""
+    """Given an n x n 2D matrix, rotate it
+    90 degrees clockwise
+
+    @Args:
+        matrix (nested list): The 2D Matrix
+
+    @Returns:
+        matrix (nested list): The rotated 2D Matrix
+    """
     for i in range(len(matrix)):
         for j in range(i, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
